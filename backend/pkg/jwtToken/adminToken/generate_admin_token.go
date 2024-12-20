e56f254d32b7ddb8d0bb4eed2283f53daf765885
phn00dev <hudayberdipolat@gmail.com>
2024-12-20 16:55:56 +0500

    created generate admin token and generate user token functions

--- backend/pkg/jwtToken/adminToken/generate_admin_token.go
+++ backend/pkg/jwtToken/adminToken/generate_admin_token.go
@@ -1,1 +1,44 @@
 package adminToken
+
+import (
+	"errors"
+	"github.com/golang-jwt/jwt"
+	"time"
+)
+
+const SecretAdminKey = "HJsGYdUDF!DN^Bdd$%asj*_da#sd!has$$ash#dasd&%^$@"
+
+type AdminClaims struct {
+	AdminID uint `json:"admin_id"`
+	jwt.StandardClaims
+}
+
+func GenerateAdminToken(adminID uint) (string, error) {
+	expirationTime := time.Now().Add(2 * time.Hour)
+	claims := &AdminClaims{
+		AdminID: adminID,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString([]byte(SecretAdminKey))
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
+
+func ValidateAdminToken(tokenString string) (*AdminClaims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &AdminClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(SecretAdminKey), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	if claims, ok := token.Claims.(*AdminClaims); ok && token.Valid {
+		return claims, nil
+	}
+
+	return nil, errors.New("invalid token")
+}
